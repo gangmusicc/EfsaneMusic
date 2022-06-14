cb615611d5553e22f8d94c21f37c55dcd9c02ddb
gangmusicc <107477993+gangmusicc@users.noreply.github.com>
2022-06-15 01:21:26 +0300

    Update and rename handlers/pmpermit.py to işleyiciler/pmpermit.py

--- işleyiciler/pmpermit.py
+++ işleyiciler/pmpermit.py
@@ -18,7 +18,7 @@ async def pmPermit(client: USER, message: Message):
                 return
             await USER.send_message(
                 message.chat.id,
-                "Merhaba, Ben bir @rollroysmusic_bot asistanıyım. Herhangi bir sorun yaşıyorsanız sahibim @emiriviaa ile iletişime geçiniz.",
+                "Merhaba, Ben bir @Gangalarmultimbot asistanıyım. Herhangi bir sorun yaşıyorsanız sahibim @bendilemma ya da @ImEylul iletişime geçiniz.",
             )
             return
  
