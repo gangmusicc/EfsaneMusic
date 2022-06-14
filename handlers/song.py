6b434ecb3df13758e3e15fc98e40e275a0f6a4d5
gangmusicc <107477993+gangmusicc@users.noreply.github.com>
2022-06-14 14:49:23 +0300

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -55,7 +55,7 @@ def bul(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"☑️ **İsmi**: [{title[:35]}]({link})\nDaha fazlası için ☞ @BirazdaMuzik"
+        rep = f"☑️ **İsmi**: [{title[:35]}]({link})\n⭐️ **Tarafından:** @gangalarradyo"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
